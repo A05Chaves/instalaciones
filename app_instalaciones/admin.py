943eb9fb2b9e8ff207d49fc6956d8f005d65d502
Alfredo <figo12747@gmail.com>
2025-02-12 22:13:47 -0500

    trabajo en formulario de chequeo servir 2

--- app_instalaciones/admin.py
+++ app_instalaciones/admin.py
@@ -1,17 +1,5 @@
 from django.contrib import admin
 from app_instalaciones.models.cuadroInstalaciones import cuadroInst
 
-"""
-class cuadroInstalacionesInline(admin.TabularInline):
-    model = cuadroInstalaciones
-
-
-# Register your models here.
-class cuadroInstalacionesAdmin(admin.ModelAdmin):
-    list_display= ['pvg', 'cliente','fecha']
-    
-admin.site.register(cuadroInstalaciones, cuadroInstalacionesAdmin)
-"""
-
 #DE ESTA FORMA LA BASE QUE AGREGAMOS YA APARECE EN EL PANEL ADMINISTRATIVO
 admin.site.register(cuadroInst)
