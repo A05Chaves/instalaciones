251a5ff7d067b45ea8c3c9cfc6ccd0af5d9db096
jachavesr <54504439+jachavesr@users.noreply.github.com>
2026-06-10 17:08:45 -0500

    actualizacion sistema

--- instalaciones/settings.py
+++ instalaciones/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "app_instalaciones.context_processors.permisos_usuario",
             ],
         },
     },
